e625e09ae3ee292c2b65b33d0153873539b1eb1a
npardom <npardom@unal.edu.co>
2023-03-05 16:54:02 -0500

    footerAdded

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import milhojasLogo from "../assets/milhojas-logo.png";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footerText">Una página de</div>
+      <img src={milhojasLogo} className="milhojasLogo" />
+    </div>
+  );
+}
+
+export default Footer;
