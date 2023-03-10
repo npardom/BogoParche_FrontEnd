07f86b73ed3d1e49dea04bed261330d6a77896ef
Nicolas Pardo Machett's <npardom@unal.edu.co>
2023-03-10 18:20:04 -0500

    firstAttemptAtConnecting

--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -1,6 +1,17 @@
+import {useState, useEffect} from "react";
+
 function Catalogue() {
+  const [dato, setDato] = useState(null);
+
+  useEffect(() => {
+    console.log(
+    fetch("/api")
+      .then((res) => res.json())
+      .then((dato) => setDato(dato.data)));
+  }, []);
+
   return (
-    <div>catalogue</div>
+    <div>{!dato ? "Loading..." : dato}</div>
   )
 }
 
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,20 +1,76 @@
-function SignUp(){
-    return <div className="card">
-        <div className = "cardContainer">
-        <div className = "titleCard">Registrarse</div>
-      <form>
-        <input placeholder = "Nombre de Usuario" className = "loginField">
-        </input>
-        <input placeholder = "Correo electrónico" className = "loginField">
-        </input>
-        <input placeholder = "Contraseña" type = "password" className = "loginField">
-        </input>
-        <input placeholder = "Confirmar contraseña" type = "password" className = "loginField">
-        </input>
-        <button className = "loginButton2">Crear cuenta</button>
-      </form>
-        </div>
+import { useState, useEffect } from "react";
+
+function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+
+  const getUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+  const getEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const getPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const getPasswordAgain = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordAgain(event.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password === passwordAgain) {
+      console.log(username, email, password, passwordAgain);
+      const body = {username, email, password}
+      console.log(body);
+      //fetch("/api/signup", {
+      //  method: "POST",
+      //  mode: "cors",
+      //  body: JSON.stringify(body),
+      //});
+    } else {
+      console.log("passwords don't match");
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className="cardContainer">
+        <div className="titleCard">Registrarse</div>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={getUsername}
+            value={username}
+            placeholder="Nombre de Usuario"
+            className="loginField"
+          ></input>
+          <input
+            onChange={getEmail}
+            value={email}
+            placeholder="Correo electrónico"
+            className="loginField"
+          ></input>
+          <input
+            onChange={getPassword}
+            value={password}
+            placeholder="Contraseña"
+            type="password"
+            className="loginField"
+          ></input>
+          <input
+            onChange={getPasswordAgain}
+            value={passwordAgain}
+            placeholder="Confirmar contraseña"
+            type="password"
+            className="loginField"
+          ></input>
+          <button className="loginButton2">Crear cuenta</button>
+        </form>
+      </div>
     </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
