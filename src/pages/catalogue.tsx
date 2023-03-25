7342ab3cdd4215f5a2aa9e0a3728dedfb5a78f41
EstebanUmba <eumbarila@unal.edu.co>
2023-03-24 22:25:33 -0500

    First push for filters

--- src/pages/catalogue.tsx
+++ src/pages/catalogue.tsx
@@ -13,13 +13,61 @@ function Catalogue() {
 
   return (
     <div className ="catalogueContainer">
-    <div className = "catalogueCardsContainer">
-      {activities.map((activity: Activity)=> (
-        <ActivityCard activity = {activity}/>
-      ))}
-    </div>
+      <div className ="catalogueSearchBar">
+        <form>
+          <input type="search"
+              placeholder = "Ingresa términos de búsqueda" className = "searchField" required>
+          </input>
+          <button className = "searchButton">&#x1F50D;</button>
+        </form>
+      </div>
+      {/*<div className="favsContainer">
+        <label>
+          <input type ="checkbox">
+          </input> Favoritos
+        </label>
+      </div>
+      <div className="assistContainer">
+        <label>
+          <input type ="checkbox">
+          </input> Eventos a asistir
+        </label>
+      </div>*/}
+      <div className="catalogueContainer2">
+        <div className="filterContainer">
+          <p className="filterTitle">
+            Categorías
+          </p>
+          <label className="filterItem">
+            <input type ="checkbox">
+            </input> Entretenimiento
+          </label>
+          <label className="filterItem">
+            <input type ="checkbox">
+            </input> Gastronomía
+          </label>
+          <p className="filterTitle">
+            Precios
+          </p>
+          <label className="filterItem">
+            <input type ="checkbox">
+            </input> Gratis
+          </label>
+          <label className="filterItem">
+            <input type ="checkbox">
+            </input> 1k - 10k
+          </label>
+        </div>
+        <div className = "catalogueCardsContainer">
+          {activities.map((activity: Activity)=> (
+            <ActivityCard activity = {activity}/>
+          ))}
+        </div>
+      </div>
     </div>
   )
+
+  
 }
 
 export default Catalogue
